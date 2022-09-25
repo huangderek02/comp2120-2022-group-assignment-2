5f615a568e27e1780737453d8a02fbdda82194b0
u7165475 <u7165475@anu.edu.au>
2022-09-25 12:48:27 +1000

    added javafx

--- courage-rpg/src/main/GUI/Viewer.java
+++ courage-rpg/src/main/GUI/Viewer.java
@@ -1,4 +1,28 @@
-@Override
-public void start(Stage primaryStage) {
+package main.GUI;
 
-        }
+import javafx.application.Application;
+
+/**
+ * @author Xin Lu
+ */
+public class Viewer extends Application {
+
+    // Game window is of size 1080p x 720p
+    private static final int VIEWER_WIDTH = 1080;
+    private static final int VIEWER_HEIGHT = 720;
+
+    private final Group root = new Group();
+
+    /**
+     * Entry pointer for the game.
+     */
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Courage");
+        Scene scene = new Scene(root, VIEWER_WIDTH, VIEWER_HEIGHT);
+
+        // Start the application
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}

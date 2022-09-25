c0b0a45acbab52dbbc49284a96df64c7adbf6ab3
u7165475 <u7165475@anu.edu.au>
2022-09-25 12:48:27 +1000

    added javafx

--- courage-rpg/src/main/java/engine/Cell.java
+++ courage-rpg/src/main/java/engine/Cell.java
@@ -7,22 +7,25 @@ import java.util.List;
 
 /**
  * Cell represents a cell in a scene
+ *
  * @author Xianghao Wang
- * */
+ */
 public abstract class Cell {
 
     /**
      * Build cell with a list of arguments configured in JSON file
-     * @author Xianghao Wang
+     *
      * @param arguments: represents a list of arguments
      * @return a cell built with these arguments
-     * */
+     * @author Xianghao Wang
+     */
     public abstract Cell build(List<String> arguments);
 
     /**
      * This defines the behaviour of this cell, this may change the game state
-     * @author Xianghao Wang
+     *
      * @param state the whole game state
-     * */
+     * @author Xianghao Wang
+     */
     public abstract void act(GameState state);
 }

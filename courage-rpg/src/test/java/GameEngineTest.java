668012c5e1d50e7f43e1543a534bf14372d1ef6a
Xianghao Wang <u7321615@anu.edu.au>
2022-09-30 15:30:33 +1000

    update test: add GameEngineTest

---
+++ courage-rpg/src/test/java/GameEngineTest.java
@@ -0,0 +1,24 @@
+import engine.GameEngine;
+import javafx.util.Pair;
+import org.junit.Test;
+
+import java.net.URISyntaxException;
+
+import static org.junit.Assert.*;
+
+public class GameEngineTest {
+    @Test
+    public void getResourcePathTest() throws URISyntaxException {
+    }
+
+
+    @Test
+    public void getJSONObjectTest() {
+
+    }
+
+    @Test
+    public void parseArgumentTest() {
+
+    }
+}

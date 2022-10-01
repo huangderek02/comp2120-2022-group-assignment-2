9375e5fc7552c1151286879b6737434227462a29
u7165475 <u7165475@anu.edu.au>
2022-10-01 17:39:40 +1000

    updated GUI

--- courage-rpg/src/main/java/GUI/Main.java
+++ courage-rpg/src/main/java/GUI/Main.java
@@ -1,11 +1,15 @@
 package GUI;
 
+import engine.Cell;
 import engine.GameEngine;
 import engine.GameObject;
 import javafx.application.Application;
 import javafx.scene.control.Label;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.util.Pair;
+
+import java.util.List;
 
 /**
  * @author Xin Lu
@@ -17,13 +21,17 @@ public class Main extends Application {
     GameObject gameObject ;
     Controller controller;
 
-    GameEngine engine;
+    GameEngine engine = new GameEngine();
 
     @Override
-    public void start(Stage stage) {
+    public void start(Stage stage) throws ClassNotFoundException {
         viewer = new Viewer();
         controller = new Controller(viewer, null);
         controller.init();
         controller.handleKeyBoard();
+
+        String str = "model.cells.WallCell&123&hello";
+        Pair<Class<Cell>, List<String>> actual = GameEngine.parseArgument(str);
+        System.out.println(actual);
     }
 }
